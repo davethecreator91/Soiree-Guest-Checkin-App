4b6d9237528fd8217764a5f8acbb62b9346f80fa
dbryant <d.bryant718@gmail.com>
2024-11-19 14:13:12 -0500

    pages running

--- server.js
+++ server.js
@@ -11,34 +11,85 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method")); // new
 app.use(morgan("dev")); //new
-app.use(express.static(path.join(__dirname, "public"))); // serves static files from directory
+// app.use(express.static(path.join(__dirname, "public"))); // serves static files from directory
 
+//Import models
+const guestPassModel = require("./models/guestPass.js");
 
 mongoose.connect(process.env.MONGODB_URI); // Connect to MongoDB
 mongoose.connection.on("connected", () => { //Log connection
     console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
   });
-//Import models
-const GuestPass = require("./models/guestPass.js");
-const guestPassModel = require("./models/guestPass.js");
+
 
 //-----------------GET----------------------------
-app.get('/', async (req, res) => {
-    res.render('index.ejs')
+
+app.get("/", async (req, res) => {
+    // res.send("hello, friend!")
+    res.render("index.ejs");
+    console.log("Pulled Up Index.ejs");
+})
+
+app.get("/guestcheckin", async (req,res) => {
+    res.render("guestPass/newGuestCheckIn.ejs");
+    // res.redirect("/guestlist")    
+
 })
 
 app.get("/guestlist", async (req,res) => {
     const allGuests = await guestPassModel.find();
-    res.render("guestPass/index.ejs", {guests: allGuests});
+    res.render("guestPass/guestlist.ejs", {guests: allGuests});
 })
+
+app.get("/guestlist/:guestId", async (req,res) => {
+    const foundGuest = await guestPassModel.findById(req.params.guestId);
+    res.render("guestPass/show.ejs", { guest: foundGuest });
+});
+
 //-----------------POST---------------------------
+
+app.post("/guestlist", async (req,res) => {
+    //General Pass checkbox
+    if (req.body.generalPassAccess === "on") {
+        req.body.generalPassAccess = true;
+    } else {
+        req.body.generalPassAccess = false;
+    }
+    //Sports Pass checkbox
+    if (req.body.sportsPassAccess === "on") {
+        req.body.sportsPassAccess = true;
+    } else {
+        req.body.sportsPassAccess = false;
+    }
+    //Premier Pass checkbox
+    if (req.body.premierPassAccess === "on") {
+        req.body.premierPassAccess = true;
+    } else {
+        req.body.premierPassAccess = false;
+    }
+    //All Access Pass checkbox
+    if (req.body.allAccessPass === "on") {
+        req.body.allAccessPass = true;
+    } else {
+        req.body.allAccessPass = false;
+    }
+    await guestPassModel.create(req.body);
+    console.log(req.body);
+});
+
 //-----------------DELETE-------------------------
+
+app.delete("/guestlist/:guestId", async (req,res) => {
+    await guest.findByIdAndDelete(req.params.guestId);
+
+    res.redirect(`/guestlist/${req.params.fruitId}`);
+})
 //-----------------UPDATE-------------------------
 
 
 
 //Ports
-app.listen(3008, () => {
-    console.log("Listening on port 3008");
+app.listen(3007, () => {
+    console.log("Listening on port 3007");
   });
 
