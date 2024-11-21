8d0fcec135c0a6894eb773b79824ad5339da0449
dbryant <d.bryant718@gmail.com>
2024-11-21 10:25:25 -0500

    Updated

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(morgan("dev")); //new
 // app.use(express.static(path.join(__dirname, "public"))); // serves static files from directory
 
 //Import models
-const guestPassModel = require("./models/guestPass.js");
+const guestPassModel = require("./models/guestModel.js");
 
 mongoose.connect(process.env.MONGODB_URI); // Connect to MongoDB
 mongoose.connection.on("connected", () => { //Log connection
@@ -38,51 +38,64 @@ app.get("/guestcheckin", async (req,res) => {
 
 app.get("/guestlist", async (req,res) => {
     const allGuests = await guestPassModel.find();
-    res.render("guestPass/guestlist.ejs", {guests: allGuests});
+    res.render("guestPass/guestlist.ejs", {guestsID: allGuests});
 })
 
 app.get("/guestlist/:guestId", async (req,res) => {
     const foundGuest = await guestPassModel.findById(req.params.guestId);
-    res.render("guestPass/show.ejs", { guest: foundGuest });
+    res.render("guestPass/show.ejs", { guestID: foundGuest });
 });
 
 //-----------------POST---------------------------
 
 app.post("/guestlist", async (req,res) => {
-    //General Pass checkbox
+
+});
+
+app.post("/guestCheckIn", async (req,res) => {
     if (req.body.generalPassAccess === "on") {
         req.body.generalPassAccess = true;
+        req.body.passType = 'General Pass';
     } else {
         req.body.generalPassAccess = false;
     }
     //Sports Pass checkbox
     if (req.body.sportsPassAccess === "on") {
         req.body.sportsPassAccess = true;
+        req.body.passType = 'Sports Pass';
     } else {
         req.body.sportsPassAccess = false;
     }
     //Premier Pass checkbox
     if (req.body.premierPassAccess === "on") {
         req.body.premierPassAccess = true;
+        req.body.passType = 'Premier Pass';
     } else {
         req.body.premierPassAccess = false;
     }
     //All Access Pass checkbox
     if (req.body.allAccessPass === "on") {
         req.body.allAccessPass = true;
+        req.body.passType = 'All Access Pass';
     } else {
         req.body.allAccessPass = false;
     }
-    await guestPassModel.create(req.body);
+
+
+
+    const guestCheckedIn = await guestPassModel.create(req.body);
     console.log(req.body);
+    res.redirect("/guestlist");
+    
 });
+
 
 //-----------------DELETE-------------------------
 
 app.delete("/guestlist/:guestId", async (req,res) => {
     await guest.findByIdAndDelete(req.params.guestId);
 
-    res.redirect(`/guestlist/${req.params.fruitId}`);
+    res.redirect(`/guestlist/${req.params.guestId}`);
 })
 //-----------------UPDATE-------------------------
 
